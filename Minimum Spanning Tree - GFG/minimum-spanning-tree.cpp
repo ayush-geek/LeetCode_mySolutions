f98aede78a539d093cc40673d12f5f603267cb86
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-01-11 20:02:21 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,144 +3,39 @@
 using namespace std;
 
 // } Driver Code Ends
-
-
- class DSU
-{
-private: 
-    vector<int> rank, parent, size;
-  
-public:
-    DSU(int n)
-    {
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        size.resize(n+1,1);
-
-        for(int i=0;i<=n;i++)
-            parent[i]=i;
-    };
-   
-    int findUPar(int u)
-    {
-        if(u==parent[u])
-            return u;
-
-        return parent[u]=findUPar(parent[u]);
-    }
-
-    void unionByRank(int u,int v)
-    {
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-
-        if(ulp_u==ulp_v)   
-            return ;
-
-        if(rank[ulp_u]<rank[ulp_v])
-        {
-            parent[ulp_u]=ulp_v;
-            //rank[ulp_v]++;
-            
-        }
-        else  if(rank[ulp_u]>rank[ulp_v])
-        {
-            parent[ulp_v]=ulp_u;
-            // rank[ulp_u]++;
-        }
-        else{
-             parent[ulp_u]=ulp_v;
-            rank[ulp_v]++;
-        }
-
-    }
-
-
-     void unionBySize(int u,int v)
-    {
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-
-        if(ulp_u==ulp_v)   
-            return ;
-
-        if(size[ulp_u]<=size[ulp_v])
-        {
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-            
-        }
-        else
-        {
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-
-    }
-
-
-};
-
-
-
 class Solution
 {
-	
-	
-	
-	
-	
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	
-	
-	
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-     
-        DSU  ds(V);
-        
-        
-        vector<pair<int,pair<int,int>>> edges;
-        
-        
-        for(int i=0;i<V;i++)
-        {
-        for(auto itr: adj[i])
-        {
-            int v=itr[0];
-            int wt=itr[1];
+       
+        vector<int> vis(V,0);
+       priority_queue<pair<int,int>> pq;
+       pq.push({0,0});
+       
+       int sm=0;
+       while(!pq.empty())
+       {
+           auto it=pq.top();
+           int wt=it.first;
+           int u=it.second;
+         
            
-            
-            //cout<<wt<<endl;
-            edges.push_back({wt,{i,v}});
-            
-            
-            
-        }
-        }
-        
-        sort(edges.begin(),edges.end());
-        
-        int ans=0;
-        for(auto it: edges)
+           pq.pop();
+           
+           if(vis[u]==1)
+            continue;
+            sm+=abs(wt);
+           vis[u]=1;
+           
+           for(auto it: adj[u])
         {
-            int wt=it.first;
-            int u=it.second.first;
-            int v=it.second.second;
-            
-            
-            if(ds.findUPar(u)!=ds.findUPar(v))
-            {
-                //cout<<wt<<endl;
-                ans+=wt;
-                
-                ds.unionBySize(u,v);
-            }
-            
-        }
-        
-        return ans;
+               pq.push({-it[1],it[0]});
+           }
+       }
+       
+       return sm;
     }
 };
 
