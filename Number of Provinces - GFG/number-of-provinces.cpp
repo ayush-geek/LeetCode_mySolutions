8d73b75b44b5b160358e225596484253685c7c07
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-01-22 21:16:25 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -6,117 +6,38 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class DSU
-{
-private: 
-    vector<int> rank, parent, size;
-  
-public:
-    DSU(int n)
-    {
-        rank.resize(n+1,0);
-        parent.resize(n+1);
-        size.resize(n+1,1);
-
-        for(int i=0;i<=n;i++)
-            parent[i]=i;
-    };
-   
-    int findUPar(int u)
-    {
-        if(u==parent[u])
-            return u;
-
-        return parent[u]=findUPar(parent[u]);
-    }
-
-    void unionByRank(int u,int v)
-    {
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-
-        if(ulp_u==ulp_v)   
-            return ;
-
-        if(rank[ulp_u]<rank[ulp_v])
-        {
-            parent[ulp_u]=ulp_v;
-            //rank[ulp_v]++;
-            
-        }
-        else  if(rank[ulp_u]>rank[ulp_v])
-        {
-            parent[ulp_v]=ulp_u;
-            // rank[ulp_u]++;
-        }
-        else{
-             parent[ulp_u]=ulp_v;
-            rank[ulp_v]++;
-        }
-
-    }
-
-
-     void unionBySize(int u,int v)
-    {
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-
-        if(ulp_u==ulp_v)   
-            return ;
-
-        if(size[ulp_u]<=size[ulp_v])
-        {
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-            
-        }
-        else
-        {
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-
-    }
-
-
-};
-
-
-
 class Solution {
   public:
-    int numProvinces(vector<vector<int>> adj, int V) {
+  
+    void dfs(int i,vector<vector<int>>& adj,vector<int>& vis)
+    {
         
-        int n=V;
-        DSU ds(V);
+        vis[i]=1;
         
-        for(int i=0;i<n;i++)
+        for(int j=0;j<adj[i].size();j++)
         {
-            for(int j=0;j<n;j++)
+            if(!vis[j] && adj[i][j]==1)
             {
-                if(i!=j &&  adj[i][j]==1)
-                {
-                    int pi=ds.findUPar(i);
-                    int pj=ds.findUPar(j);
-                    
-                    if(pi!=pj)
-                    {
-                        ds.unionBySize(i,j);
-                    }
-                }
+                dfs(j,adj,vis);
             }
         }
-       int ans=0; 
-        
+    }
+  
+    int numProvinces(vector<vector<int>> adj, int V) {
+       
+        vector<int> vis(V,0);
+        int ans=0;
         for(int i=0;i<V;i++)
         {
-            if(ds.findUPar(i)==i)
+            if(!vis[i])
+            {
+                dfs(i,adj,vis);
                 ans++;
+            }
         }
-        
+    
         return ans;
-    }
+    }   
 };
 
 //{ Driver Code Starts.
