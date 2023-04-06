c61dac2bbb3501c1589f52265278ec2f4e639d7c
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-04-06 16:36:36 +0530

    Added solution - LeetHub

--- Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -13,32 +13,48 @@ class Solution {
     *   V: number of vertices
     */
     vector<int> bellman_ford(int V, vector<vector<int>>& edges, int S) {
-        // Code here
+       
         
-        vector<int> dist(V,1e8);
-        dist[S]=0;
+        vector<int> dis(V,1e8);
+        dis[S]=0;
         
-        for(int i=0;i<V;i++)
-        {
-            for(auto it: edges)
-            {
-                int u=it[0];
-                int v=it[1];
-                int wt=it[2];
-                
-                
-                if(dist[u]!=1e8 && dist[u]+wt<dist[v])
-                {
-                    dist[v]=dist[u]+wt;
-                    
-                    if(i==V-1)
-                        return {-1};
-                }
-            }
-        }
+       for(int i=0;i<V-1;i++)
+       {
+           for(auto& ele: edges)
+           {
+           int u=ele[0];
+           int v=ele[1];
+           int w=ele[2];
+           
+           if(dis[u]+w<dis[v])
+           {
+               dis[v]=dis[u]+w;
+           }
+           }
+       }
+        int fg=0;
+        for(auto& ele: edges)
+           {
+           int u=ele[0];
+           int v=ele[1];
+           int w=ele[2];
+           
+           if(dis[u]+w<dis[v])
+           {
+               dis[v]=dis[u]+w;
+                fg=1;
+                break;
+               
+           }
+          
+               
+           }
+           
+           if(fg==1)
+            return {-1};
         
-        return dist;
-        
+        return dis;       
+       
     }
 };
 
