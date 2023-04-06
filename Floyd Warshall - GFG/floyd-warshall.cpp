884dcc761338bead872a0aef3a7140f3672cdead
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-04-06 16:57:20 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -11,21 +11,19 @@ class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
 	    
-	    
 	    int n=matrix.size();
+	    int m=matrix[0].size();
 	    
 	    for(int i=0;i<n;i++)
 	    {
-	        for(int j=0;j<n;j++)
+	        for(int j=0;j<m;j++)
 	        {
 	            if(matrix[i][j]==-1)
-	                matrix[i][j]=1e9;
-	                
-	               if(i==j)
-	                matrix[i][j]=0;
+	                   matrix[i][j]=1e9;
+	            if(i==j)
+	                matrix[i][i]=0;
 	        }
 	    }
-	    
 	    
 	    for(int via=0;via<n;via++)
 	    {
@@ -33,24 +31,23 @@ class Solution {
 	        {
 	            for(int j=0;j<n;j++)
 	            {
-	               
-	                    matrix[i][j]=min(matrix[i][via]+matrix[via][j],matrix[i][j]);
+	                matrix[i][j]=min(matrix[i][j],matrix[i][via]+matrix[via][j]);
 	            }
 	        }
 	    }
 	    
 	    
-	     for(int i=0;i<n;i++)
+	      
+	    for(int i=0;i<n;i++)
 	    {
-	        for(int j=0;j<n;j++)
+	        for(int j=0;j<m;j++)
 	        {
 	            if(matrix[i][j]==1e9)
-	                matrix[i][j]=-1;
-	                
-	               if(i==j)
-	                matrix[i][j]=0;
+	                   matrix[i][j]=-1;
+	          
 	        }
 	    }
+	    
 	}
 };
 
