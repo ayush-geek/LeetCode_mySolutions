93f4c5f620737864d848ea83977f697eddcfc5a7
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-07-05 18:53:09 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -5,54 +5,45 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int mod= 1e9 + 7;
+    int mod=1e9+7;
+	int perfectSum(vector<int>& nums, int n, int sum)
+	{
+        // Your code goes here
+	  int tgt=sum;
+	 vector<vector<int>> dp(n+1,vector<int> (tgt+1,0));
+
+    // for(int i=0;i<n;i++)
+    // {
+    //     dp[i][0]=1;
+    // }
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=tgt;j++)
+            {
+                if(nums[i-1]<=j)
+                    dp[i][j]=dp[i-1][j-nums[i-1]];
+                
+                dp[i][j]=(dp[i][j]+dp[i-1][j])%mod;
+               
+            }
+          
+        }
+	  
+	    return dp[n][sum];
+	}
+    
     int countPartitions(int n, int d, vector<int>& arr) {
         // Code here
         
-      int sum=accumulate(arr.begin(),arr.end(),0);
-	    vector<vector<int>> dp(n+1,vector<int>  (sum+1,0));
-            
-            //for(int i=0;i<=n;i++)
-                dp[0][0]=1;
-                
-            
-            for(int i=1;i<=n;i++)
-            {
-                for(int j=0;j<=sum;j++)
-                {
-                    if(j>=arr[i-1])
-                        dp[i][j]=dp[i-1][j-arr[i-1]];
-                    
-                    dp[i][j]=(dp[i][j] + dp[i-1][j])%mod;
-                }
-            }
-           
-            int ans=0;
-           for(int j=0;j<=sum;j++)
-           {
-               if(dp[n][j])
-               {
-                   int s1=j;
-                int s2=sum-j;
-                
-                if(s1-s2==d)
-                {
-                    ans+=dp[n][j];
-                }
-
-               }
-           }
-           
-          
-                return ans;
+        int s=accumulate(arr.begin(),arr.end(),0);
         
-   
+        int tgt=(d+s)/2;
         
-    
-        
-      
-      
-        
+        if((d+s)%2!=0)
+            return 0;
+        // cout<<tgt<<endl;
+    return perfectSum(arr,n,tgt);
         
     }
 };
