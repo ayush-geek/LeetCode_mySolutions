2a6d226ca4941b6c9a7fa09e1c2a67e839e89009
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-07-08 22:26:36 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,37 +3,81 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DSU{
+    
+    public:
+    vector<int> parent, size;
+    
+    DSU(int n)
+    {
+        parent.resize(n+1,0);
+        size.resize(n+1,1);
+        iota(parent.begin(),parent.end(),0);
+    }
+    
+    int findUpar(int nd)
+    {
+        if(parent[nd]==nd)
+            return nd;
+            
+        return parent[nd]=findUpar(parent[nd]);
+    }
+    
+    bool unionBySize(int a,int b)
+    {
+        int pa=findUpar(a);
+        int pb=findUpar(b);
+        
+        if(pa==pb)
+            return false;
+        
+        if(size[pa]<size[pb])
+        {
+            size[pb]+=size[pa];
+            parent[pa]=pb;
+        }
+        else
+        {
+           size[pa]+=size[pb];
+            parent[pb]=pa; 
+        }
+        
+        return true;
+    }
+    
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        
-       pq.push({0,0});
-       vector<int> vis(V,0);
-        int ans=0;
-       while(!pq.empty())
-       {
-           auto d=pq.top().first;
-           auto nd=pq.top().second;
-           pq.pop();
-       
-           
-           if(vis[nd]==1)
-            continue;
-            ans+=d;
-            vis[nd]=1;
-            
-            for(auto& ele: adj[nd])
-            {
-                if(!vis[ele[0]])
-                    pq.push({ele[1],ele[0]});
-            }
-       }
-       
-       return ans;
+      vector<pair<int,pair<int,int>>> vpp;
+      vector<int> vis(V,0);
+      DSU ds(V);
+      for(int i=0;i<V;i++)
+      {
+          for(auto& ele: adj[i])
+          {
+              vpp.push_back({ele[1],{ele[0],i}});
+          }
+      }
+      
+      sort(vpp.begin(),vpp.end());
+      int ans=0;
+      
+      for(auto& ele: vpp)
+      {
+          int a=ele.first;
+          int b=ele.second.first;
+          int c=ele.second.second;
+          
+          if(ds.unionBySize(b,c))
+            ans+=a;
+      }
+      return ans;
     }
 };
 
