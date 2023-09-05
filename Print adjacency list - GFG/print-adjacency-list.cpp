3759c9b96ec14c0ff239d1a40d3e5a4a77b9124b
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-09-05 08:32:46 +0530

    Added solution - LeetHub

--- Print adjacency list - GFG/print-adjacency-list.cpp
+++ Print adjacency list - GFG/print-adjacency-list.cpp
@@ -6,17 +6,21 @@ using namespace std;
 class Solution {
   public:
     // Function to return the adjacency list for each vertex.
-    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
         
-        vector<vector<int>> g;
+        vector<vector<int>> res(V);
         
-        for(int i=0;i<V;i++)
+        
+        for( auto& ele: edges)
         {
-            adj[i].insert(adj[i].begin(),i);
-            g.push_back(adj[i]);
+            res[ele.first].push_back(ele.second);
+             res[ele.second].push_back(ele.first);
+            
         }
         
-        return g;
+        return res;
+        
     }
 };
 
@@ -27,21 +31,20 @@ int main() {
     while (tc--) {
         int V, E;
         cin >> V >> E;
-        vector<int> adj[V];
+        vector<pair<int,int>>edges;
         for (int i = 0; i < E; i++) {
             int u, v;
             cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            edges.push_back({u,v});
         }
         Solution obj;
-        vector<vector<int>> ans = obj.printGraph(V, adj);
-        for (int i = 0; i < ans.size(); i++) {
-            for (int j = 0; j < ans[i].size() - 1; j++) {
-                cout << ans[i][j] << "-> ";
-            }
-            cout << ans[i][ans[i].size() - 1];
-            cout << endl;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        for(int i=0;i<V;i++)
+        {
+            sort(adj[i].begin(),adj[i].end());
+            for(auto it:adj[i])
+                cout<<it<<" ";
+            cout<<endl;
         }
     }
     return 0;
