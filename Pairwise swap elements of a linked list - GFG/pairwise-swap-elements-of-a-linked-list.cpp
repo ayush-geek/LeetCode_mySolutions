bb244ab02241d5b3f5502bb72bc9ca5cb6898521
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-10-07 09:15:29 +0530

    Added solution - LeetHub

---
+++ Pairwise swap elements of a linked list - GFG/pairwise-swap-elements-of-a-linked-list.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+class Solution
+{
+    public:
+    Node* pairWiseSwap(struct Node* head) 
+    {
+        // The task is to complete this method
+         Node* tmp=NULL;
+         Node* prev=NULL;
+        while(head!=NULL && head->next!=NULL)
+        {
+            // swap(head->data,head->next->data);
+            Node* t1=head->next;
+            Node* t2=head->next->next;
+            Node* cur=head;
+            if(prev!=NULL)
+                prev->next=t1;
+            else
+                tmp=t1;
+            t1->next=cur;
+            cur->next=t2;
+            head=t2;
+            prev=cur;
+        }
+        if(tmp==NULL)
+            return head;
+        return tmp;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
+}
+
+// } Driver Code Ends
