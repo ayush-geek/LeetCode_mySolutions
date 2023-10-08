4d3c812ae7d53f5075e51fa9b5a367e6489ede56
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2023-10-08 10:07:00 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+        // Code here
+        if(head->data>data)
+        {
+            Node* tmp=new Node(data);
+            tmp->next=head;
+            return tmp;
+        }
+            
+        Node* cur=head;
+        int fg=0;
+        Node* prev=NULL;
+        while(cur!=NULL)
+        {
+            if(cur->data>data)
+            {   
+                 Node* tmp=new Node(data);
+                prev->next=tmp;
+                tmp->next=cur;
+                fg=1;
+                break;
+            }
+            prev=cur;
+            cur=cur->next;
+        }
+        
+        if(fg==0)
+        {
+            prev->next=new Node(data);
+        
+            
+        }
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
