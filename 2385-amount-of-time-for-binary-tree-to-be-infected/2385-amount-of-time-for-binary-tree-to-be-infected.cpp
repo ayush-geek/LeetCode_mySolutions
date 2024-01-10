feebb0094133f0d511479795ed15ed20758b2c00
Ayush Malviya <87216570+ayush-geek@users.noreply.github.com>
2024-01-10 23:10:36 +0530

    Time: 451 ms (45.25%), Space: 187.4 MB (23.99%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void createGraph(TreeNode* root, int start , unordered_map<int,vector<int>> &adjacecny){
+        queue<pair<TreeNode*,int>>q;
+
+        q.push({root,-1});
+
+        while(!q.empty()){
+            TreeNode* currNode=q.front().first;
+            int parent=q.front().second;
+            q.pop();
+            if(parent==-1) {
+                 if(currNode->left) q.push({currNode->left,currNode->val});
+                 if(currNode->right) q.push({currNode->right,currNode->val});
+                 continue;
+            }
+            adjacecny[parent].push_back(currNode->val);
+            adjacecny[currNode->val].push_back(parent);
+            
+            if(currNode->left) q.push({currNode->left,currNode->val});
+            if(currNode->right) q.push({currNode->right,currNode->val});
+        }
+    
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        // We can' create here adjacnecy list, which we create normally in graph , because here we not know the exact nodes value, as it is not continuous, So unordered_map to store the values.
+        unordered_map<int,vector<int>> adjacecny;
+        createGraph(root,start,adjacecny);
+        queue<pair<int,int>> q;
+        unordered_map<int,bool>vis;
+        int t=0;
+        vis[start]=true;
+        q.push({start,0});
+         while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            int node = it.first;
+            int time = it.second;
+
+            t=max(t,time);
+
+            for(auto it:adjacecny[node]){
+                if(!vis[it]){
+                    q.push({it,time+1});
+                    vis[it]=true;
+                }
+            }
+        }
+        return t;
+
+    }
+};
